bf142416de8eb36fe82ce7c84433b8a9570fcd01
career-yashaswee <career.yashaswee@gmail.com>
2025-10-01 19:46:49 +0530

    add(stage)

---
+++ web/app/stage/layout.tsx
@@ -0,0 +1,23 @@
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+import { SiteHeader } from "@/components/site-header";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="[--header-height:calc(--spacing(14))]">
+      <SidebarProvider className="flex flex-col">
+        <SiteHeader />
+        <div className="flex flex-1">
+          <AppSidebar />
+          <SidebarInset>
+            <div className="flex flex-1 flex-col gap-4 p-4">{children}</div>
+          </SidebarInset>
+        </div>
+      </SidebarProvider>
+    </div>
+  );
+}
