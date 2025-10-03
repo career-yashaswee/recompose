04d703a5cdeada6e8445f958dff574dd8b596988
career-yashaswee <career.yashaswee@gmail.com>
2025-10-03 23:18:55 +0530

    fix(deployment)

--- web/scripts/websocket-server.js
+++ web/scripts/websocket-server.js
@@ -2,7 +2,7 @@
 
 import { WebSocketServer } from "ws";
 import http from "http";
-import { PrismaClient } from "../app/generated/prisma/index.js";
+import { PrismaClient } from "../app/generated/prisma";
 
 const prisma = new PrismaClient();
 
